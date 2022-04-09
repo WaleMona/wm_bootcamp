fab0ffbbbca24b315f9783813e28c87045a35060
Wale Mona <walemona21@outlook.com>
2022-04-09 18:17:34 -0400

    Initial commit of bootcamp utilities.

---
+++ jb_bootcamp/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for utilities for bootcamp."""
+
+from .na_utils import *
+from .bioinfo_dicts import *
+
+__author__ = 'Wale Mona'
+__email__ = 'walemona21@outlook.com'
+__version__ = '0.0.1'
